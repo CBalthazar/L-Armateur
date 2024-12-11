246d95ec12fe324a5ba6e2c6d2aa2eaab03bfca3
CBalthazar <balthclement@gmail.com>
2024-12-11 15:12:19 +0100

    grabbing data on form submission

--- connection/connection.js
+++ connection/connection.js
@@ -1,13 +1,39 @@
 const main = document.querySelector("main");
-const flipCard = document.querySelector(".flip-card");
-const toggleInscription = main.insertBefore(
-  document.createElement("div"),
-  flipCard
-);
 
-let array = [("Connection", "Inscription")];
-array.forEach((element) => {
-  let el = toggleInscription.appendChild(document.createElement("div"));
-  el.textContent = element;
-  el.style.padding = "1em";
+let toggleBtn = document.getElementById("toggler");
+let loginForm = document.getElementById("login");
+let signinForm = document.getElementById("signin");
+let [loginSubmitBtn, signinSubmitBtn] = document.querySelectorAll("button");
+
+loginForm.classList.add("hidden");
+
+toggleBtn.addEventListener("click", () => {
+  document
+    .querySelectorAll("main div div")
+    .forEach((x) => x.classList.toggle("selected"));
+  loginForm.classList.toggle("hidden");
+  signinForm.classList.toggle("hidden");
+});
+
+loginSubmitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  let inputs = document.querySelectorAll("#login input");
+  inputs.forEach((input) => {
+    console.log(input.name);
+    console.log(input.value);
+  });
+  let div = main.appendChild(document.createElement("div"));
+  div.textContent = "we got you, you're well logged in";
+});
+
+signinSubmitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  let inputs = document.querySelectorAll("#signin input");
+  console.log(inputs);
+  inputs.forEach((input) => {
+    console.log(input.name);
+    console.log(input.value);
+  });
+  let div = main.appendChild(document.createElement("div"));
+  div.textContent = "we got you, you might receive an email soon, who nose";
 });
