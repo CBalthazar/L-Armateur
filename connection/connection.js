c6ab66a8e4018bb8092b4182b10498a8a46b92cb
Balthazar <90700196+CBalthazar@users.noreply.github.com>
2024-12-17 19:54:36 +0100

    Feature/favorites (#3)
    * note somewhat working
    
    * made working new, load, save buttons
    
    * getting ideas straight
    
    * finished with notes for now
    
    * files for faving added
    
    * styling
    
    * icon fix, navbar fix
    
    * on ne parlera pas de tout ces commits mal placés X(
    
    * fetching correctly

--- connection/connection.js
+++ connection/connection.js
@@ -1,12 +1,20 @@
+// ### Functions ###
+function addUsers(users) {
+  let storedUsers = localStorage.getItem("users") || [];
+  storedUsers.push(...users);
+  localStorage.setItem("users", storedUsers);
+}
+
+// ### Variables ###
 const main = document.querySelector("main");
 
 let toggleBtn = document.getElementById("toggler");
 let loginForm = document.getElementById("login");
 let signinForm = document.getElementById("signin");
 let [loginSubmitBtn, signinSubmitBtn] = document.querySelectorAll("button");
-
 loginForm.classList.add("hidden");
 
+// ### Event Manager ###
 toggleBtn.addEventListener("click", () => {
   document
     .querySelectorAll("main div div")
@@ -28,12 +36,71 @@ loginSubmitBtn.addEventListener("click", (event) => {
 
 signinSubmitBtn.addEventListener("click", (event) => {
   event.preventDefault();
-  let inputs = document.querySelectorAll("#signin input");
-  console.log(inputs);
-  inputs.forEach((input) => {
-    console.log(input.name);
-    console.log(input.value);
-  });
+  let inputs = Array.from(document.querySelectorAll("#signin input"));
+  inputs.forEach((input) => input.value.trim());
+  const emptyFields = inputs.filter((input) => !input.value.length);
+
+  //currently fields order hardcoded cause no time, to change later
+
+  // empty field verification
+  if (emptyFields.length) {
+    let div = main.appendChild(document.createElement("div"));
+    div.textContent =
+      "you missed the following fields : " +
+      emptyFields.map((input) => input.name).join(", ");
+    div.classList.add("warn");
+    return;
+  }
+
+  // quick check for names
+  let validationRegex = /^[^0-9!@#$%^&*]+$/;
+  for (let input of inputs.slice(0, 2)) {
+    if (!input.value.match(validationRegex)) {
+      let div = main.appendChild(document.createElement("div"));
+      div.textContent = "Sorry, one of your name fields appear invalid to me.";
+      div.textContent +=
+        "\nEither you tried playing with me or I was made wrong";
+      div.classList.add("warn");
+      return;
+    }
+  }
+
+  // check email valid
+  validationRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!inputs.at(2).value.trim().match(validationRegex)) {
+    let div = main.appendChild(document.createElement("div"));
+    div.textContent = "email is not valid";
+    div.classList.add("warn");
+    return;
+  }
+
+  // password valid
+  validationRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+  if (!inputs.at(-2).value.trim().match(validationRegex)) {
+    let div = main.appendChild(document.createElement("div"));
+    div.textContent = "password is not valid, must contain at least ";
+    div.classList.add("warn");
+    return;
+  }
+
+  // password confirmation
+  if (inputs.at(-2).value !== inputs.at(-1).value) {
+    let div = main.appendChild(document.createElement("div"));
+    div.textContent = "password and confirmation are not identical";
+    div.classList.add("warn");
+    return;
+  }
+
+  addUsers([
+    {
+      name: inputs[0],
+      surname: inputs[1],
+      email: inputs[2],
+      password: inputs[3],
+    },
+  ]);
+
   let div = main.appendChild(document.createElement("div"));
   div.textContent = "we got you, you might receive an email soon, who nose";
+  div.classList.add("info");
 });
---
+++ notes/notes.js
@@ -0,0 +1,139 @@
+function newNote(text = "", title = "") {
+  // make new note, delete current if exists
+  newNotediv.innerHTML = "";
+  const note = newNotediv.appendChild(document.createElement("div"));
+  const divTitle = document.createElement("div");
+  const divText = document.createElement("div");
+  const titleArea = divTitle.appendChild(document.createElement("textarea"));
+  const textArea = divText.appendChild(document.createElement("textarea"));
+
+  titleArea.setAttribute("placeholder", "Add Title...");
+  textArea.setAttribute("placeholder", "Add Text...");
+  titleArea.classList.add("titleArea");
+  textArea.classList.add("textArea");
+  titleArea.value = title;
+  textArea.value = text;
+
+  note.classList.add("note");
+  note.append(divTitle, divText);
+}
+
+function loadNotes(container) {
+  container.innerHTML = "";
+  let storedNotes = loadStorage("notes", defaultObject);
+  console.log(storedNotes);
+  //
+  storedNotes[id].forEach((noteObject) => {
+    let note = container.appendChild(document.createElement("div"));
+    note.classList.add("note");
+    let title = note.appendChild(document.createElement("h2"));
+    let text = note.appendChild(document.createElement("p"));
+    title.textContent = noteObject.title;
+    text.textContent = noteObject.text;
+  });
+}
+
+function saveNote() {
+  //get note content
+  const textareas = document.querySelectorAll("textarea");
+  if (!textareas.length) {
+    return;
+  }
+  let note = {};
+  note.title = textareas[0].value;
+  note.text = textareas[1].value;
+
+  let storedNotes = loadStorage("notes", defaultObject);
+  storedNotes[id].push(note);
+  localStorage.setItem("notes", JSON.stringify(storedNotes));
+  loadNotes(notes);
+}
+
+function loadStorage(key, defaultObject) {
+  try {
+    let stored = JSON.parse(localStorage.getItem(key));
+    if (stored) {
+      return stored;
+    }
+  } catch (error) {
+    console.log("Error while loading localStorage Data :\n" + error);
+  }
+  return defaultObject;
+}
+
+// Code Execution
+// localStorage.setItem("notes", "false"); //local rest, testing purposes, to be removed, change
+
+// Constants
+const main = document.querySelector("main");
+const newNotediv = document.querySelector(".new-note");
+const newNoteBtn = document.querySelector(".add-note");
+const saveNoteBtn = document.querySelector(".save-note");
+const loadNoteBtn = document.querySelector(".load-note");
+const notes = document.querySelector(".notes");
+
+const id = "../Assets/Tests/Starry Night"; //for now, to change
+// set default result shape, to change maybe, I'll ask Marine
+let defaultObject = {};
+defaultObject[id] = [];
+
+loadNotes(notes);
+console.log("loading complete");
+
+// Binding buttons to events
+newNoteBtn.addEventListener("click", () => {
+  // anonymous function so event is not passed in argument
+  newNote();
+});
+newNoteBtn.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    event.target.click();
+  }
+});
+
+loadNoteBtn.addEventListener("click", () => {
+  // anonymous function so event is not passed in argument
+  loadNotes(notes);
+});
+loadNoteBtn.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    event.target.click();
+  }
+});
+
+saveNoteBtn.addEventListener("click", () => {
+  // anonymous function so event is not passed in argument
+  saveNote();
+});
+saveNoteBtn.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    event.target.click();
+  }
+});
+
+/* page at start:
+=> header:
+<>the Website Logo/Name
+<> all navigation buttons
+=> art piece on the left
+|> take the most space it can
+=> notes on the right
+=> initialise localStorage if no previous notes
+|> must load preexisting notes
+|> must show new notes on create
+=> buttons to make new notes
+
+// Events
+=> On Clicks
+  => new note
+    => make new emty note
+      => stylised text areas for 
+        => title
+        => text
+    => keep existing note in their right place
+  => load notes
+      => stylised card for each
+*/
